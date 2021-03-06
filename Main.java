7e697f1c0b2eef32d435e937f8203e89c6505431
Victoria Holland <JavaDevVictoria@users.noreply.github.com>
2021-03-06 20:18:23 +0000

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by victoria on 06/03/21.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Cookie cookie = new Cookie();
+        cookie.calculateIngredients(cookie.getNumberOfCookies());
+        cookie.printRecipeMethod();
+    }
+}
