378c9bc8926b2ffbf9c1b12204eae884f2c8d512
Victoria Holland <JavaDevVictoria@users.noreply.github.com>
2021-03-07 23:03:53 +0000

    Update and rename Cookie.java to RecipeMethodPrinter.java

---
+++ RecipeMethodPrinter.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+/**
+ * Created by victoria on 06/03/21.
+ */
+
+public class RecipeMethodPrinter {
+
+    public void printRecipeMethod() {
+        //Leave gap between ingredients and method
+        System.out.println(" ");
+
+        //Print out the recipe method
+        System.out.println("Step 1 : Mix sugar, butter and "
+                + "self raising flour thoroughly.");
+        System.out.println("Step 2 : Stir in egg, a few "
+                + "drops of vanilla essence and chocolate chips.");
+        System.out.println("Step 3 : Roll out mixture and "
+                + "cut out cookies.");
+        System.out.println("Step 4 : Bake in hot oven "
+                + "(Gas Mark 6) for 10-12 minutes.");
+    }
+}
